ffe499f9120f63a44137d35ec04e3e3d7dbb43af
avag <avag.novembit@gmail.com>
2019-06-06 15:16:36 +0400

    add taxonomy to connection cpt artguments via hook

--- includes/hooks.php
+++ includes/hooks.php
@@ -10,6 +10,7 @@ function setup() {
 		'init',
 		function () {
 			add_action( 'dt_meta_box_external_connection_details', __NAMESPACE__ . '\add_external_connection_details', 10, 1 );
+			add_filter( 'dt_before_setup_cpt', __NAMESPACE__ . '\add_taxonomy_to_cpt_args' );
 		}
 	);
 }
@@ -22,4 +23,18 @@ function setup() {
 function add_external_connection_details( $post ) {
 	$connection_groups = \Distributor\ExternalConnectionGroups::factory();
 	$connection_groups->display_groups( $post->ID );
+}
+
+/**
+ * Add taxonomy to the connection custom post type arguments
+ *
+ * @param array $cpt_args
+ *
+ * @return array
+ */
+function add_taxonomy_to_cpt_args( $cpt_args ) {
+
+	$cpt_args['taxonomies'] = [ 'dt_ext_connection_group' ];
+
+	return $cpt_args;
 }
